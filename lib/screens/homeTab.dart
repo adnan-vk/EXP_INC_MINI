84485499568750336a5b82392a48ba30bd94ce95
Adnan-vk <adnanadn32@gmail.com>
2023-11-25 15:26:50 +0530

    edited

--- lib/screens/homeTab.dart
+++ lib/screens/homeTab.dart
@@ -15,48 +15,73 @@ class _hometabState extends State<hometab> {
         centerTitle: true,
         title: Text("TRANSACTIONS"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(height: 10,),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-                  labelText: "Search Here....",
+      body: Stack(
+        children: [
+          Container(
+        color: Colors.grey,
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            TextFormField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(30)
                 ),
+                filled: true,
+                fillColor: Color.fromARGB(255, 123, 117, 117),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+                labelText: "Search Here....",labelStyle: TextStyle(color: Colors.white)
               ),
-              SizedBox(height: 10,),
-              Text("Your Current Balance"),
-              Text("49236",style: TextStyle(fontSize: 25),),
-
-              ListView.builder(itemBuilder: (context, index){
-                Container(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text("Salary"),
-                          Text("50000")
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("Get the salary"),
-                          Text("01-05-2023"),
-                        ],
-                      )
-                    ],
+            ),
+            SizedBox(height: 10,),
+            Text("Your Current Balance"),
+            Text("49236",style: TextStyle(fontSize: 25),),
+            Expanded(
+              child: ListView.builder(itemBuilder: (context, index){
+               return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Salary"),
+                            Text("50000")
+                          ],
+                        ),
+                        SizedBox(height: 40,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Get the salary"),
+                            Text("01-05-2023"),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 );
               },
               itemCount: 50,
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
-      );
+              Positioned(
+                bottom: 15,
+                right: 15,
+                child: FloatingActionButton.extended(
+                  icon: Icon(Icons.add),
+                  onPressed: (){}, 
+                label: Text("Add Transaction")),
+              ),
+            
+    ],
+  )
+  );
   }
 }
