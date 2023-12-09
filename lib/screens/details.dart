7c82349d5c3fa2e3d5f2c41deec4bc6184059c90
Adnan-vk <adnanadn32@gmail.com>
2023-12-09 16:20:23 +0530

    edited

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -24,73 +24,77 @@ class _DetailsState extends State<Details> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("DETAILS"),
         backgroundColor:  const Color.fromARGB(255, 3, 45, 79),
       ),
-      body: Container(
-        color: Colors.grey,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 50
-              ),
-              child: Container(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          color: Colors.grey,
+          child: Column(
+            children: [
+              Padding(
                 padding: const EdgeInsets.only(
-                  top: 50,
-                  bottom: 50,
-                  left: 25,right: 25
+                  left: 20,
+                  right: 20,
+                  top: 50
                 ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white,
-                ),
-                
-                child:  Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("ITEM",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
-                      Column(
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    top: 50,
+                    bottom: 50,
+                    left: 25,right: 25
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+                  
+                  child:  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(widget.decr,style: const TextStyle(fontWeight: FontWeight.w600),),
+                          const Text("ITEM",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
+                        Column(
+                          children: [
+                            Text(widget.decr,style: const TextStyle(fontWeight: FontWeight.w600),),
+                          ],
+                        )
                         ],
-                      )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Date",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
-                        Text(widget.date),
-                      ],
-                    ),
-                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Amount",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
-                        Text(widget.amount,style: const TextStyle(fontWeight: FontWeight.w600),),
-                      ],
-                    ),
-                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Type",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
-                        Text(widget.type,style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: widget.type == 'INCOME'? Colors.green : widget.type == "EXPENCE"? Colors.red : Colors.black),),
-                      ],
-                    ),
-                  ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Date",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
+                          Text(widget.date),
+                        ],
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Amount",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
+                          Text(widget.amount,style: const TextStyle(fontWeight: FontWeight.w600),),
+                        ],
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Type",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600)),
+                          Text(widget.type,style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: widget.type == 'INCOME'? Colors.green : widget.type == "EXPENCE"? Colors.red : Colors.black),),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Lottie.asset("assets/anm2.json")
-          ],
+              Lottie.asset("assets/anm2.json",height: height*0.6)
+            ],
+          ),
         ),
       )
         
